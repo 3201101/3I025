11d55cee177885f76b808f93f06f1cced8c991c9
Benlog <loglisci@hotmail.fr>
2016-03-08 04:09:27 +0100

    Continuation du A*

--- Partie 2/teaching-iaro/pySpriteWorld-forStudents/algorithme heuristique Aetoile.py
+++ Partie 2/teaching-iaro/pySpriteWorld-forStudents/algorithme heuristique Aetoile.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
+def manathan((x,y),(i,j)):
+    return abs(i-x) + abs(j-y)
+
 class APathFinder:
     
-    def __init__(self, init, goal, wall, heur = self.manathan):
+    def __init__(self, init, goal, wall, heur = manathan):
         self.init = init
         self.goal = goal
         self.wall = wall
@@ -10,26 +13,25 @@ class APathFinder:
         self.seen = []
         self.heuristique = heur
         
-        self.path = algo()
+        self.path = self.algo()
         self.iter = 0
         
     def algo(self):
-        while(len(frontier) != 0):
+        while(len(self.frontier) != 0):
             self.updateFronier()
-            seen.add(frontier.pop(0))
+            self.seen.add(self.frontier.pop(0))
             
     
     def updateFrontier(self):
         (x,y) = self.frontier[0]
         for (i,j) in [(0,-1),(1,0),(-1,0),(0,1)]:
-            if((x+i,y+j) not in wall) and ((x+i,y+j) not in frontier) and self.isInBorder(x+i,y+j):
-                if (x+i,y+j) in seen:
-                    if 
-                    self.frontier.append((x+i,y+j))
+            pt = (x+i,y+j)
+            if(pt not in self.wall) and (pt not in self.frontier) and self.isNotOutside(pt):
+                if (pt) in self.seen:
+                    if self.seen[list.index(pt)][2] > self.heuristique(pt,self.goal):
+                        self.frontier.append((pt,self.heuristique(pt,self.goal)))
                     
     
-    def isInBorder(self, x, y):
-        return x>=0 and x<=20 and y>=0 and y<=20
+    def isNotOutside(self, pt):
+        return pt[0]>=0 and pt[0]<=20 and pt[1]>=0 and pt[1]<=20
             
-    def manathan((x,y),(i,j)):
-        return abs(i-x) + abs(j-y)
